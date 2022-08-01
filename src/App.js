d1042a2b693a3be82f1534f883bbad957bc85f98
suame2021 <85235483+suame2021@users.noreply.github.com>
2022-08-01 23:20:07 +0100

    mrr

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 //import logo from './logo.svg';
 import NavbarComponent from "./navbarComponent";
 import "bootstrap/dist/css/bootstrap.min.css"
+import HomeComponent from "./home";
+import CarouselComponent from "./carouselComponent";
+
 
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent/>
+      <CarouselComponent/>
+      <HomeComponent/>
 
 
     </div>
---
+++ src/home.js
@@ -0,0 +1,25 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+
+
+const HomeComponent = () => {
+    return ( 
+
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+            </Container>
+            
+        </div>
+
+        
+     );
+}
+ 
+export default HomeComponent;
